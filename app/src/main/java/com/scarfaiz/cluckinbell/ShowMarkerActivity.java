c5c644630f79582415e18bdf53ef8c1221481272
Jeavie <dejeaview@gmail.com>
2017-10-09 23:04:26 +0300

    New layout

---
+++ app/src/main/java/com/scarfaiz/cluckinbell/ShowMarkerActivity.java
@@ -0,0 +1,17 @@
+package com.scarfaiz.cluckinbell;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jeavie on 09/10/2017.
+ */
+
+public class ShowMarkerActivity extends Activity  {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_marker);
+    }
+}
