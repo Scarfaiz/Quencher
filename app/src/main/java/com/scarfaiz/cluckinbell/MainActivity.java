46c2cb1e14143c1d51a5c082f91e0258ff69f0c0
DrKestrel <higgskestrel@gmail.com>
2017-10-11 19:04:34 +0300

    New permission checker

--- app/src/main/java/com/scarfaiz/cluckinbell/MainActivity.java
+++ app/src/main/java/com/scarfaiz/cluckinbell/MainActivity.java
@@ -3,7 +3,9 @@ package com.scarfaiz.cluckinbell;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -15,7 +17,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Toast;
 
@@ -29,10 +34,15 @@ import org.osmdroid.views.overlay.MapEventsOverlay;
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends Activity {
 
     LocationManager locationManager;
-    MapView map;
+    public static MapView map;
     IMapController mapController;
     public boolean geodata_updated = false;
     MyLocationNewOverlay oMapLocationOverlay;
@@ -41,34 +51,41 @@ public class MainActivity extends Activity {
     BitmapFactory.Options person_options;
     GeoPoint startPoint;
     FloatingActionButton LocButton;
+    int permsRequestCode;
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         Configuration.getInstance().setUserAgentValue("CB");
         setContentView(R.layout.activity_main);
+        permsRequestCode = 1;
+        GetLocPermission();
         map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setMultiTouchControls(true);
         mapController = map.getController();
-        mapController.setZoom(15);
-        startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setZoom(13);
+        startPoint = new GeoPoint(59.93863, 30.31413);
         mapController.animateTo(startPoint);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         //location marker overlay
         oMapLocationOverlay = new MyLocationNewOverlay(map);
-        /*person_options = new BitmapFactory.Options();
-        person_options.inSampleSize = 4;
-        person_drawable = getResources().getDrawable(R.drawable.marker_cluster);
-        person_bitmap = ((BitmapDrawable) person_drawable).getBitmap();
-        oMapLocationOverlay.setPersonIcon(person_bitmap);*/
         oMapLocationOverlay.enableMyLocation();
-        oMapLocationOverlay.enableFollowLocation();
+        //oMapLocationOverlay.enableFollowLocation();
         oMapLocationOverlay.setDrawAccuracyEnabled(true);
         oMapLocationOverlay.setDirectionArrow(BitmapFactory.decodeResource(getResources(), R.drawable.direction_arrow),
                 BitmapFactory.decodeResource(getResources(), R.drawable.direction_arrow));
         map.getOverlays().add(oMapLocationOverlay);
+        //map.postInvalidate();
+        /*person_options = new BitmapFactory.Options();
+        person_options.inSampleSize = 4;
+        person_drawable = getResources().getDrawable(R.drawable.ic_menu_mylocation);
+        person_bitmap = ((BitmapDrawable) person_drawable).getBitmap();
+        oMapLocationOverlay.setPersonIcon(person_bitmap);
+        if (oMapLocationOverlay.isEnabled())
+            Toast.makeText(getBaseContext(),"Enabled", Toast.LENGTH_LONG).show();*/
         //rotation
         RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(ctx, map);
         mRotationGestureOverlay.setEnabled(true);
@@ -79,8 +96,11 @@ public class MainActivity extends Activity {
         if (permission == PermissionChecker.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, myLocationListener);
+            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        } else {
+            GetLocPermission();
         }
-        else GetLocPermission();
+
         //setting listener for locButton
         LocButton = findViewById(R.id.locButton);
         LocButton.setOnClickListener(new View.OnClickListener() {
@@ -88,11 +108,14 @@ public class MainActivity extends Activity {
             public void onClick(View view) {
                 int permission = PermissionChecker.checkSelfPermission(MainActivity.this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
                 if (permission == PermissionChecker.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, myLocationListener);
                     Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                     updateLoc(lastLocation);
+                    if (mapController.zoomIn())
+                        mapController.zoomOut();
                     return;
-                }
-                else {
+                } else {
                     GetLocPermission();
                     return;
                 }
@@ -124,7 +147,7 @@ public class MainActivity extends Activity {
     }
 
 
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -132,10 +155,11 @@ public class MainActivity extends Activity {
         //Configuration.getInstance().save(this, prefs);
         int permission = PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
         if (permission == PermissionChecker.PERMISSION_GRANTED) {
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, myLocationListener);
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-            return;}
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, myLocationListener);
+            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+            return;
+        }
     }
 
     @Override
@@ -145,24 +169,25 @@ public class MainActivity extends Activity {
         locationManager.removeUpdates(myLocationListener);
     }
 
-    private void updateLoc(Location loc){
+    private void updateLoc(Location loc) {
         int permission = PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
         if (permission == PermissionChecker.PERMISSION_GRANTED) {
-        GeoPoint locGeoPoint = new GeoPoint(loc.getLatitude(), loc.getLongitude());
-        mapController.animateTo(locGeoPoint);
-        map.invalidate();}
-        else GetLocPermission();
+            GeoPoint locGeoPoint = new GeoPoint(loc.getLatitude(), loc.getLongitude());
+            mapController.animateTo(locGeoPoint);
+        } else GetLocPermission();
 
     }
 
     private LocationListener myLocationListener
-            = new LocationListener(){
+            = new LocationListener() {
 
         @Override
         public void onLocationChanged(Location location) {
             // TODO Auto-generated method stub
-            if(!geodata_updated){
+            if (!geodata_updated) {
                 updateLoc(location);
+                mapController.zoomTo(17);
+                map.invalidate();
                 geodata_updated = true;
             }
         }
@@ -177,6 +202,7 @@ public class MainActivity extends Activity {
         public void onProviderEnabled(String provider) {
             // TODO Auto-generated method stub
 
+
         }
 
         @Override
@@ -186,8 +212,93 @@ public class MainActivity extends Activity {
         }
 
     };
+
     public void GetLocPermission() {
-        Toast.makeText(MainActivity.this, "Для работы приложения необходимо предоставить разрешение на использование геопозиции", Toast.LENGTH_LONG).show();
-        startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            checkAndRequestPermissions();
+        } else {
+            Toast.makeText(MainActivity.this, "Для работы приложения необходимо предоставить разрешение на использование геопозиции", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
+        }
+    }
+
+    private boolean checkAndRequestPermissions() {
+        int permissionSendMessage = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int locationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int storagePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (locationPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (permissionSendMessage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (storagePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), permsRequestCode);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 200: {
+
+                Map<String, Integer> perms = new HashMap<>();
+                // Initialize the map with both permissions
+                perms.put(Manifest.permission.ACCESS_COARSE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with actual results from user
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++)
+                        perms.put(permissions[i], grantResults[i]);
+                    // Check for both permissions
+                    if (perms.get(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            && perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        // process the normal flow
+                        //else any one or both the permissions are not granted
+                        //waitAndNavigateToOnboardingTutorial();
+                    } else {
+                        //permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
+                        // shouldShowRequestPermissionRationale will return true
+                        // show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                            showDialogOK("Camera, Storage and Location Services Permission required for this app", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    switch (which) {
+                                        case DialogInterface.BUTTON_POSITIVE:
+                                            checkAndRequestPermissions();
+                                            break;
+                                        case DialogInterface.BUTTON_NEGATIVE:
+                                            // proceed with logic by disabling the related features or quit the app.
+                                            break;
+                                    }
+                                }
+                            });
+                        } //permission is denied (and never ask again is checked)
+                        // shouldShowRequestPermissionRationale will return false else
+                        {
+                            Toast.makeText(this, "Go to settings and enable permissions", Toast.LENGTH_LONG).show(); // //proceed with logic by disabling the related features or quit the app.
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private void showDialogOK(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", okListener)
+                .create()
+                .show();
     }
 }
