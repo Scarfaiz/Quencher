5455b9b915eb80f75102f76312b266f5d21e9fa2
DrKestrel <higgskestrel@gmail.com>
2017-10-28 02:24:28 +0300

    -xml parsing WORKS!!!!

--- app/src/main/java/com/scarfaiz/cluckinbell/XMLParser.java
+++ app/src/main/java/com/scarfaiz/cluckinbell/XMLParser.java
@@ -1,6 +1,5 @@
 package com.scarfaiz.cluckinbell;
 
-import android.util.Log;
 import android.util.Xml;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -31,92 +30,21 @@ public class XMLParser {
         }
     }
 
-    private String readCurrency(XmlPullParser parser, String requirement) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, ns, requirement);
-        String mTempStr;
-        String result = null;
-        Log.d("LogDebug", "////////parsing in " + requirement);
-        while (parser.getEventType() != XmlPullParser.END_TAG) {
-            switch (parser.getEventType()) {
-                // если это начало документа
-                case XmlPullParser.START_DOCUMENT:
-                    Log.d("LogDebug", "START_DOCUMENT");
-                    break;
-                // если это начало тэга
-                case XmlPullParser.START_TAG:
-                    if(parser.getName()!=null) if(parser.getName().equals("result")) result = parser.getName();
-                    Log.d("LogDebug", "START_TAG: имя тэга = " + parser.getName()
-                            + ", глубина = " + parser.getDepth() + ", число атрибутов = "
-                            + parser.getAttributeCount());
-                    mTempStr  = "";
-                    for (int i = 0; i < parser.getAttributeCount(); i++) {
-                        mTempStr = mTempStr + parser.getAttributeName(i) + " = "
-                                + parser.getAttributeValue(i) + ", ";
-                    }
-                    if (!mTempStr.isEmpty())
-                        Log.d("LogDebug", "Атрибyты: " + mTempStr);
-                    break;
-                // если это конец тэга
-                case XmlPullParser.END_TAG:
-                    Log.d("LogDebug", "END_TAG: имя = " + parser.getName());
-                    break;
-                // если это содержимое тэга
-                case XmlPullParser.TEXT:
-                    if(parser.getName()!=null)parser.require(XmlPullParser.START_TAG, ns, parser.getName());
-                    Log.d("LogDebug", "текст = " + parser.getText());
-                    break;
-
-                default:
-                    break;
-            }
-            // переходим к следующему элементу
-            parser.next();
-        }
-        Log.d("LogDebug", "////////finished parsing in " + requirement);
-        return result;
-    }
-
     private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
         List entries = new ArrayList();
-        String mTempStr;
-        boolean r = true;
-        while (parser.getEventType() != XmlPullParser.END_DOCUMENT && r) {
-            switch (parser.getEventType()) {
-                // если это начало документа
-                case XmlPullParser.START_DOCUMENT:
-                    Log.d("LogDebug", "START_DOCUMENT");
-                    break;
-                // если это начало тэга
-                case XmlPullParser.START_TAG:
-                    Log.d("LogDebug", "START_TAG: имя тэга = " + parser.getName()
-                            + ", глубина = " + parser.getDepth() + ", число атрибутов = "
-                            + parser.getAttributeCount());
-                    mTempStr  = "";
-                    for (int i = 0; i < parser.getAttributeCount(); i++) {
-                        mTempStr = mTempStr + parser.getAttributeName(i) + " = "
-                                + parser.getAttributeValue(i) + ", ";
-                    }
-                    if (!mTempStr.isEmpty())
-                        Log.d("LogDebug", "Атрибyты: " + mTempStr);
-                    break;
-                // если это конец тэга
-                case XmlPullParser.END_TAG:
-                    Log.d("LogDebug", "END_TAG: имя = " + parser.getName());
-                    break;
-                // если это содержимое тэга
-                case XmlPullParser.TEXT:
-                    Log.d("LogDebug", "текст = " + parser.getText());
-                    break;
 
-                default:
-                    break;
+        parser.require(XmlPullParser.START_TAG, ns, "reversegeocode");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
             }
-            if(parser.getName()!=null)
-                entries.add(readCurrency(parser, parser.getName()));
-            // переходим к следующему элементу
-            parser.next();
-            if(parser.getName()!=null) if((parser.getEventType() != XmlPullParser.END_TAG && parser.getName().equals("addressparts")))
-                r = false;
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("addressparts")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
         }
         return entries;
     }
@@ -135,5 +63,79 @@ public class XMLParser {
 
     // Parses the contents of an entry. If it encounters a city, road, or house_number tag, hands them off
 // to their respective "read" methods for processing. Otherwise, skips the tag.
+    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "addressparts");
+        String city = null;
+        String road = null;
+        String house_number = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("city")) {
+                city = readCity(parser);
+            } else if (name.equals("road")) {
+                road = readRoad(parser);
+            } else if (name.equals("house_number")) {
+                house_number = readHouseNumber(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Entry(city, road, house_number);
+    }
+
+    // Processes city tags in the feed.
+    private String readCity(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "city");
+        String city = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "city");
+        return city;
+    }
+
+    // Processes House_number tags in the feed.
+    private String readHouseNumber(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "house_number");
+        String city = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "house_number");
+        return city;
+    }
+
+    // Processes road tags in the feed.
+    private String readRoad(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "road");
+        String road = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "road");
+        return road;
+    }
+
+    // For the tags city and road, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
 }
 
